120f579f68e3d5151c70f3a2be613a1b036d79e5
ndanilop <danilodn993@gmail.com>
2026-06-21 21:29:05 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/vis-dados-trab-final/',
   // Diz ao Vite para tratar a pasta 'static' como a raiz do servidor
   publicDir: 'static' 
 });
